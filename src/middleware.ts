c04fc3631ce504873364679095aa22fcb14d6f44
brunoocn <bruno.caneo@contemplato.com>
2025-03-09 02:08:12 -0300

    feat: criando estrutura

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+// import { NextResponse } from 'next/server';
+// import { NextRequest } from 'next/server';
+// import { fetchWrapper } from './utils/fetchWrapper';
+
+import { NextRequest } from "next/server";
+
+export default async function middleware(request: NextRequest) {
+
+}
